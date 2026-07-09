596e2ee17e6a06db42095fb71c8da676e8dfaee3
nandanar24 <nandanar24@gmail.com>
2026-07-09 13:08:21 +1200

    Initial Commit

---
+++ ZenSchedule/ZenScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenScheduleApp.swift
+//  ZenSchedule
+//
+//  Created by Nandana Ramachandran on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZenScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
